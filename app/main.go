bb4df5b8b3ca96cdce3e64bb1f1366978254f1d3
Meet Vyas <vyasmet@gmail.com>
2026-09-01 12:41:34 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -43,7 +43,7 @@ func main() {
 					},
 				},
 			},
-			Tools: []openai.ChatCompletionNewParamsToolUnion{
+			Tools: []openai.ChatCompletionToolUnionParam{
                 {
                     OfFunction: &openai.ChatCompletionFunctionToolParam{
                         Function: openai.FunctionDefinitionParam{
